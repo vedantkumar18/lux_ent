fb35adee3f98e71ea515f8218c8335f743669cc3
Ace-Extrenof <prateeshAce@proton.me>
2024-09-15 10:00:00 +0530

    i forgot what i did

--- docs/lux.js
+++ docs/lux.js
@@ -1,4 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("Document loaded");
-  // Add your JavaScript here
+    console.log("Document loaded");
+    // Add your JavaScript here
 });
+
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        document.getElementById("logo").style.fontSize = "25px";
+    } else {
+        document.getElementById("logo").style.fontSize = "35px";
+    }
+}
